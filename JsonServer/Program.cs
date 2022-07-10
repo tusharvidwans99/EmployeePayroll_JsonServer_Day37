49052f47ac8dac55c4cee8c552ba98a8decc6abd
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-07-10 20:20:52 +0530

    [Tushar Vidwans] Adding welcome message

---
+++ JsonServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JsonServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Json Server and RestSharp Testing for Employee Payroll");
+        }
+    }
+}
